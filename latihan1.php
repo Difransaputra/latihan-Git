4eb45b459749727df5c0912af9566a77846af669
Difransaputra <difransprt@gmail.com>
2023-09-26 04:20:49 -0700

    penambahan latihan1.php dan contoh1.php

---
+++ latihan1.php
@@ -0,0 +1,16 @@
+<?php
+
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo  "<h1> perkenalkan </h1>";
+        echo "<p> Nama  : Moch Difran Sisco Saputra </p>";
+        echo "<p> Nim   : 12220244 </p>";
+        echo "<p> Kelas : 12.3b.24 </p>";
+        echo "<p> Alamat: Kp.sempukramat </p>";
+        echo "<p> hobi  : Futsal </p>";
+        echo "<p> jurusan : SI </P>";
+             
+    }
+}    
